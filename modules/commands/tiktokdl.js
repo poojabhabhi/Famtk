b4fd07f1624b7f43b1e62a296ec0c373873cc210
Atik Hasan <73770372+ARAtik169@users.noreply.github.com>
2024-01-27 02:47:58 +0600

    Update tiktokdl.js

--- modules/commands/tiktokdl.js
+++ modules/commands/tiktokdl.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-  name: "tikdl",
+  name: "tiktok",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "Atik Hasan",
-  description: "Download TikTok videos",
-  commandCategory: "tikvideo",
-  usage: `${global.config.PREFIX}tikdl <link>`,
+  description: "tiktok videos download",
+  commandCategory: "Utilities",
+  usage: `${global.config.PREFIX}tiktok <link>`,
   usePrefix: true,
   cooldowns: 5,
 };
@@ -19,19 +19,15 @@ module.exports.run = async function({ api, event, args }) {
   try {
   const link = args[0];
   if (!link) {
-  api.sendMessage("Usage: /tikdl <link>", event.threadID);
+  api.sendMessage("Usage: /tiktok <link>", event.threadID, event.messageID);
   return;
   }
-  api.sendMessage(`Downloading...`, event.threadID);
-  const data2 = {
-    headers: {
-    'X-RapidAPI-Key': '59b9e9930amsh57f7e1367edb3c9p1c21a9jsn42c609f99290'}
-  };
-  const response = await axios.get(`https://tiktok-download-without-watermark.p.rapidapi.com/analysis?url=${encodeURIComponent(link)}`, data2);
+  api.sendMessage(`Downloading...`, event.threadID , event.messageID);
 
-  const videoUrl = response.data.data.play;
-  const title = response.data.data.title;
+  const response = await axios.get(`https://api-samir.onrender.com/tiktok?url=${encodeURIComponent(link)}`);
 
+  const videoUrl = response.data.url;
+  
   if (!videoUrl) {
   api.sendMessage("No video found for the given link.", event.threadID);
   return;
@@ -43,21 +39,21 @@ module.exports.run = async function({ api, event, args }) {
   responseType: "stream",
   });
 
-  const filePath = path.join(__dirname, "cache", "tiktok_video.mp4");
+  const filePath = path.join(__dirname, "cache", "tikvideo.mp4");
   videoResponse.data.pipe(fs.createWriteStream(filePath));
 
   videoResponse.data.on("end", () => {
   api.sendMessage(
     {
     attachment: fs.createReadStream(filePath),
-    body: `${title}`,
+    body: `Downloaded Successfully.`,
     },
-    event.threadID,
+    event.threadID ,event.messageID,
     () => fs.unlinkSync(filePath)
   );
   });
   } catch (error) {
   console.error("Error:", error);
-  api.sendMessage("An error occurred while processing the request.", event.threadID);
+  api.sendMessage("An error occurred while processing the request.", event.threadID, event.messageID);
   }
 };
